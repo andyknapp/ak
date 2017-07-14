32200b9617830ba90bbe57090a35d22926fb7916
Andy <aknapp1@gmail.com>
2017-07-13 22:18:04 -0400

    form added

---
+++ components/contact.php
@@ -0,0 +1,20 @@
+<div id="wufoo-zpn38ao167dxhw" class="contact">
+Fill out my <a href="https://aknapp.wufoo.com/forms/zpn38ao167dxhw">online form</a>.
+</div>
+<div id="wuf-adv" style="font-family:inherit;font-size: small;color:#a7a7a7;text-align:center;display:block;">Online contact and registration forms from <a href="http://www.wufoo.com">Wufoo</a>.</div>
+<script type="text/javascript">var zpn38ao167dxhw;(function(d, t) {
+var s = d.createElement(t), options = {
+'userName':'aknapp',
+'formHash':'zpn38ao167dxhw',
+'autoResize':true,
+'height':'497',
+'async':true,
+'host':'wufoo.com',
+'header':'show',
+'ssl':true};
+s.src = ('https:' == d.location.protocol ? 'https://' : 'http://') + 'www.wufoo.com/scripts/embed/form.js';
+s.onload = s.onreadystatechange = function() {
+var rs = this.readyState; if (rs) if (rs != 'complete') if (rs != 'loaded') return;
+try { zpn38ao167dxhw = new WufooForm();zpn38ao167dxhw.initialize(options);zpn38ao167dxhw.display(); } catch (e) {}};
+var scr = d.getElementsByTagName(t)[0], par = scr.parentNode; par.insertBefore(s, scr);
+})(document, 'script');</script>
---
+++ components/resume.php
@@ -0,0 +1,341 @@
+  <section class="resume">
+    <header id="masthead" class="res-header">
+      <h1 class="res-logo">Andy Knapp</h1>
+      <div class="res-contact">
+        <ul class="contact-physical">
+          <li>Wilmington, DE</li>
+        </ul>
+        <ul class="contact-digital">
+          <li><a href="tel:+19173751114">917.375.1114</a></li>
+          <li><a href="mailto:aknapp1@gmail.com">aknapp1@gmail.com</a></li>
+          <li><a href="http://andyknapp.com">andyknapp.com</a></li>
+          <li><a href="https://twitter.com/_aknapp">@_aknapp</a></li>
+        </ul>
+      </div>
+    </header>
+    <section class="experience">
+      <header class="section-title">
+        <h2>Experience</h2>
+      </header>
+      <div class="project">
+        <div class="project-details">
+          <ul>
+            <li><a href="https://seakeeper.com/">Seakeeper</a></li>
+            <li>King Design - Summer / Fall 2016</li>
+          </ul>
+        </div>
+        <div class="project-description">
+          <p>Collaborated on front end development and CMS (WordPress) integration.</p>
+        </div>
+      </div>
+      <div class="project">
+        <div class="project-details">
+          <ul>
+            <li><a href="http://www.scullycompany.com/">Scully Company</a></li>
+            <li>King Design - Summer 2016</li>
+          </ul>
+        </div>
+        <div class="project-description">
+          <p>Collaborated on front end development & WordPress Multisite implementation for Scully Company properties.</p>
+        </div>
+      </div>
+      <div class="project">
+        <div class="project-details">
+          <ul>
+            <li><a href="https://epic.church/">Epic Church</a></li>
+            <li>King Design - Summer 2016</li>
+          </ul>
+        </div>
+        <div class="project-description">
+          <p>Front end development and CMS (WordPress) integration.</p>
+        </div>
+      </div>
+
+      <div class="project">
+        <div class="project-details">
+          <ul>
+            <li><a href="http://21cccs.org/">21st Century Cyber Charter School</a></li>
+            <li>King Design - Summer 2015</li>
+          </ul>
+        </div>
+        <div class="project-description">
+          <p>Front end development and CMS (WordPress) integration.</p>
+        </div>
+      </div>
+
+      <div class="project">
+        <div class="project-details">
+          <ul>
+            <li><a href="http://usa.m3.com/">M3 USA</a></li>
+            <li>King Design - Summer 2015</li>
+          </ul>
+        </div>
+        <div class="project-description">
+          <p>Front end development and CMS (WordPress) integration.</p>
+        </div>
+      </div>
+
+      <div class="project">
+        <div class="project-details">
+          <ul>
+            <li><a href="https://www.breasthealth.org/">BreastHealth.org</a></li>
+            <li>King Design - February 2015</li>
+          </ul>
+        </div>
+        <div class="project-description">
+          <p>Front end development and CMS (WordPress) integration.</p>
+        </div>
+      </div>
+
+      <div class="project">
+        <div class="project-details">
+          <ul>
+            <li><a href="https://www.honesttea.com/">HonestTea.com</a></li>
+            <li>King Design - September 2014</li>
+          </ul>
+        </div>
+        <div class="project-description">
+          <p>Front end development and CMS (WordPress) integration.</p>
+        </div>
+      </div>
+
+      <div class="project">
+        <div class="project-details">
+          <ul>
+            <li><a href="http://www.advancephysicaltherapy.biz/">AdvancePhysicalTherapy.biz</a></li>
+            <li>Freelance - May 2014</li>
+          </ul>
+        </div>
+        <div class="project-description">
+          <p>Front end development and CMS (WordPress) integration.</p>
+        </div>
+      </div>
+
+      <div class="project">
+        <div class="project-details">
+          <ul>
+            <li><a href="http://artisancustompainting.com/">ArtisanCustomPainting.com</a></li>
+            <li>Freelance April 2014</li>
+          </ul>
+        </div>
+        <div class="project-description">
+          <p>Site design, front end development and CMS (WordPress) integration.</p>
+        </div>
+      </div>
+
+      <div class="project">
+        <div class="project-details">
+          <ul>
+            <li><a href="http://kainmurphy.com/">KainMurphy.com</a></li>
+            <li>Freelance February 2014</li>
+          </ul>
+        </div>
+        <div class="project-description">
+          <p>Site design, front end development and CMS (WordPress) integration.</p>
+        </div>
+      </div>
+
+      <div class="project">
+        <div class="project-details">
+          <ul>
+            <li><a href="http://albertoassociates.com/">AlbertoAssociates.com</a></li>
+            <li>Freelance February 2014</li>
+          </ul>
+        </div>
+        <div class="project-description">
+          <p>Site design, front end development and CMS (WordPress) integration.</p>
+        </div>
+      </div>
+
+      <div class="project">
+        <div class="project-details">
+          <ul>
+            <li><a href="http://www.frangallun.com/">FranGallun.com</a></li>
+            <li>Freelance January 2014</li>
+          </ul>
+        </div>
+        <div class="project-description">
+          <p>Site design, front end development and CMS (WordPress) integration.</p>
+        </div>
+      </div>
+
+      <div class="project">
+        <div class="project-details">
+          <ul>
+            <li><a href="http://hvwm.com/">HVWM.com</a></li>
+            <li>Freelance October 2013</li>
+          </ul>
+        </div>
+        <div class="project-description">
+          <p>Site design, front end development and CMS (WordPress) integration.</p>
+        </div>
+      </div>
+
+      <div class="project">
+        <div class="project-details">
+          <ul>
+            <li><a href="http://www.lvlrealtors.com/">LVLRealtors.com</a></li>
+            <li>Freelance April 2013</li>
+          </ul>
+        </div>
+        <div class="project-description">
+          <p>Site design & development. Integrated IDX data feed (MLS listing data) into site, resulting in one point of entry for listings.</p>
+        </div>
+      </div>
+
+      <div class="project">
+        <div class="project-details">
+          <ul>
+            <li><a href="http://fiboxusa.com/">FiboxUSA.com</a></li>
+            <li>Freelance February 2013</li>
+          </ul>
+        </div>
+        <div class="project-description">
+          <p>Site design, front end development and CMS (WordPress) integration.</p>
+        </div>
+      </div>
+
+      <div class="project">
+        <div class="project-details">
+          <ul>
+            <li><a href="http://haddonfield300.org/">Haddonfield300.org</a></li>
+            <li>Freelance October 2012</li>
+          </ul>
+        </div>
+        <div class="project-description">
+          <p>Site design, front end development and CMS (WordPress) integration.</p>
+        </div>
+      </div>
+
+      <div class="project">
+        <div class="project-details">
+          <ul>
+            <li class="res-project-title"><a href="http://www.delawarevalleysmartgrowth.org/new-site/">DelawareValleySmartGrowth.org</a></li>
+            <li>Freelance 2013</li>
+          </ul>
+        </div>
+        <div class="project-description">
+          <p>Site design, front end development and CMS (WordPress) integration.</p>
+        </div>
+      </div>
+    </section>
+    <section class="resume-skills resume-main">
+      <header class="section-title">
+        <h2>Skills / Languages</h2>
+      </header>
+      <div class="project">
+        <div class="project-details">
+          <ul>
+            <li>Skills</li>
+          </ul>
+        </div>
+        <div class="project-description">
+          <p>WordPress development & theme creation, Photoshop, Illustrator, Git & version control, build tools</p>
+        </div>
+      </div>
+      <div class="project">
+        <div class="project-details">
+          <ul>
+            <li>Languages</li>
+          </ul>
+        </div>
+        <div class="project-description">
+          <p>HTML, CSS/Sass, jQuery/JavaScript, PHP</p>
+        </div>
+      </div>
+    </section>
+    <section class="resume-misc resume-main">
+      <header class="section-title">
+        <h2>Miscellaneous</h2>
+      </header>
+      <div class="project">
+        <div class="project-details">
+          <ul>
+            <li>Growth</li>
+          </ul>
+        </div>
+        <div class="project-description">
+          <p>Attended several WordCamps and WordPress Meetups, regular reader of leading industry blogs such as CSS-Tricks, Smashing Magazine & A List Apart. Over 30 badges earned on <a href="http://teamtreehouse.com/andyknapp">Treehouse</a>.</p>
+        </div>
+      </div>
+    </section>
+    <section class="resume-prior resume-main">
+      <header class="section-title">
+        <h2>Prior Work Experience</h2>
+        <p>After college I worked in the Advertising industry, Media Planning specifically, for nearly 10 years. This experience enabled me to develop skills such as client & team management, public speaking, and an analytical thought process. I strongly believe that my background in the fast-paced and demanding advertising agency world remains a valuable and relevant experience as it relates to my current work as a Web Developer.</p>
+      </header>
+    </section>
+    <section class="resume-adv resume-main">
+      <div class="project">
+        <div class="project-details">
+          <ul>
+            <li>Maxus</li>
+            <li>New York, NY 1/08 - 8/11</li>
+            <li>Partner, Group Planning Director</li>
+            <li>Partner, Associate Planning Director</li>
+          </ul>
+        </div>
+        <div class="project-description">
+          <p>Clients: <i>Church & Dwight</i> (Trojan, Simply Saline, Arm & Hammer SpinBrush, Arm & Hammer Toothpaste), <i>Shangri-La Hotels</i></p>
+          <p>Primary senior client contact, responsible for strategic development and implementation oversight. Extensive work in the digital space, including social media strategy and custom program development.</p>
+        </div>
+      </div>
+      <div class="project">
+        <div class="project-details">
+          <ul>
+            <li>MindShare</li>
+            <li>Chicago, IL 9/05 - 3/07</li>
+            <li>Associate Planning Director</li>
+            <li>Planning Supervisor</li>
+          </ul>
+        </div>
+        <div class="project-description">
+          <p>Clients: <i>BP Lubricants</i> (Castrol Motor Oil)</p>
+          <p>Develop strategic planning for three consumer and two trade brands. Primary contact for client and partner agencies.</p>
+        </div>
+      </div>
+      <div class="project">
+        <div class="project-details">
+          <ul>
+            <li>Mediaedge</li>
+            <li>New York, NY 3/03 - 8/05</li>
+            <li>Communications Manager</li>
+            <li>Communications Analyst</li>
+          </ul>
+        </div>
+        <div class="project-description">
+          <p>Clients: <i>GlaxoSmithKline</i>, <i>AT&T</i>, <i>Remy Amerique</i></p>
+          <p>Develop strategic planning for three consumer and two trade brands. Primary contact for client and partner agencies.</p>
+        </div>
+      </div>
+      <div class="project">
+        <div class="project-details">
+          <ul>
+            <li>Brouillard Communications</li>
+            <li>New York, NY 9/00 - 3/03</li>
+            <li>Media Planner</li>
+            <li>Assistant Media Planner</li>
+          </ul>
+        </div>
+        <div class="project-description">
+        <p>Clients: <i>The Rouse Company</i>, <i>Maserati</i>, <i>Dassault Falcon</i></p>
+        </div>
+      </div>
+    </section>
+    <section class="resume-edu resume-main">
+      <header>
+        <h2>Education</h2>
+      </header>
+      <div class="project">
+        <div class="project-details">
+          <ul>
+            <li>Oswego State University</li>
+            <li>Oswego, NY 1999</li>
+          </ul>
+        </div>
+        <div class="project-description">
+          <p>Bachelor of Science, Marketing</p>
+        </div>
+      </div>
+    </section>
+  </section>
