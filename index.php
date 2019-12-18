ab94d868660dae24cb3bed97036b3efbbf284ebc
Andy <aknapp1@gmail.com>
2019-12-18 16:58:34 -0500

    file clean up

--- index.php
+++ index.php
@@ -42,7 +42,7 @@
         <footer class="site-footer" role="contentinfo">
             <ul>
                 <li class="headline-sub">&copy; <?php echo date('Y');?> | Andy Knapp</li>
-                <li class="headline-sub"><a href="/">resume pdf</a></li>
+                <li class="headline-sub"><a href="/andy-knapp-resume.pdf" target="_blank">resume pdf</a></li>
             </ul>
         </footer>
 
