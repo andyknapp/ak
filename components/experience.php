95c70ef45e25fb7e941033fdc8f91752e6edbcc4
Andy <aknapp1@gmail.com>
2019-12-18 12:12:25 -0500

    rewrite copy

--- components/experience.php
+++ components/experience.php
@@ -12,10 +12,9 @@
       </header>
 
       <div class="experience-description">
-          <p>As a web developer at King Design I strive to deliver high-performing, accessible and SEO friendly sites to the end user. I write clean, semantic front and back-end code, specializing in WordPress development. For our clients, I take pride in fully customizing the administrative area of WordPress to meet their (sometimes complex) data structure and publishing needs.</p>
-          <p>Sites I've worked on recently include; <a href="https://seakeeper.com" target="_blank" rel="nofollow">Seakeeper</a>, <a href="https://www.isapartners.org" target="_blank" rel="nofollow" rel="nofollow">ISA</a>, <a href="https://www.duncanhines.com" target="_blank" rel="nofollow">Duncan Hines</a>, <a href="https://www.birdseye.com" target="_blank" rel="nofollow">Birdseye</a>,
-              <a href="https://www.bostonbaking.com" target="_blank" rel="nofollow">Boston Baking</a>, <a href="https://abx-films.com">ABX</a> and <a href="https://kingdesignllc.com" target="_blank" rel="nofollow">King Design</a>.</p>
-          <p>Being part of a small team has afforded me the opportunity to go beyond web development and influence aspects of the entire design & development process - from planning content architecture to post-launch traffic analysis & recommendations. When time allows, I enjoy writing posts for our blog. Here are a few of my posts; <a href="https://kingdesignllc.com/blog/defining-a-critical-css-workflow-for-wordpress/" target="_blank" rel="nofollow">Defining a Critical CSS Workflow for WordPress</a>, <a href="https://kingdesignllc.com/blog/acf-gutenberg-blocks/" target="_blank" rel="nofollow">ACF Guten Blocks</a>, and <a href="https://kingdesignllc.com/blog/scrolling-the-fold/" target="_blank" rel="nofollow">Scrolling & the Fold</a>. </p>
+          <p>While a web developer at King Design I delivered high-performing, accessible and SEO friendly sites to a wide range of clients. Specializing in WordPress development, I not only focused on writing clean and semantic code for the end user, but also customizing the administrative side to meet a client's data structure and publishing needs, while empowering them to manage their site easily.</p>
+          <p>Sites worked on recently include; <a href="https://seakeeper.com" target="_blank" rel="nofollow">Seakeeper</a>, <a href="https://www.isapartners.org" target="_blank" rel="nofollow">ISA</a>, <a href="https://www.duncanhines.com" target="_blank" rel="nofollow">Duncan Hines</a>, <a href="https://www.birdseye.com" target="_blank">Birdseye</a>, <a href="https://www.bostonbaking.com" target="_blank" rel="nofollow">Boston Baking</a>, <a href="https://abx-films.com">ABX</a> and <a href="https://kingdesignllc.com" target="_blank" rel="nofollow">King Design</a>.</p>
+          <p>Being part of a small team afforded me the opportunity to go beyond web development and influence aspects of the entire design & development process - from planning content architecture to post-launch traffic analysis & recommendations. I also had the opportunity to write several blog posts. Here are a few; <a href="https://kingdesignllc.com/blog/defining-a-critical-css-workflow-for-wordpress/" target="_blank" rel="nofollow">Defining a Critical CSS Workflow for WordPress</a>, <a href="https://kingdesignllc.com/blog/acf-gutenberg-blocks/" target="_blank" rel="nofollow">ACF Guten Blocks</a>, and <a href="https://kingdesignllc.com/blog/scrolling-the-fold/" target="_blank" rel="nofollow">Scrolling & the Fold</a>. </p>
       </div>
     </section>
 
@@ -31,7 +30,7 @@
       </header>
 
       <div class="experience-description">
-          <p>Working as a freelance developer and a solo designer / developer was an invaluable experience for several reasons. During this time, I was able to greatly improve my technical skills and understanding of how WordPress and the web works. Finding and working with clients taught me how the web can work for smaller businesses and entrepreneurs, and the design work I did positively influences me as a developer to this day.</p>
+          <p>Working as a freelance developer and a solo designer / developer was an invaluable experience for several reasons. During this time I was able to greatly improve my technical skills and understanding of how WordPress, and the web works. Finding and managing clients taught me how a site can play a crucial role for smaller businesses and entrepreneurs. The design work I did, while not my strongest skillset, positively influences how I approach development and UX decisions to this day.</p>
       </div>
     </section>
 
@@ -68,9 +67,9 @@
         </header>
 
         <div class="experience-description">
-            <p>I worked in the Advertising industry, Media Planning specifically, for over 10 years. This experience enabled me to develop skills such as client & team management, public speaking, and an analytical thought process. I strongly believe that my background in the fast-paced and demanding advertising agency world remains a valuable and relevant experience as it relates to my current work as a Web Developer.</p>
+            <p>Over 10 years in the Advertising industry (Media Planning specifically) enabled me to develop strong client & team management skills, public speaking confidence, and an analytical thought process. I strongly believe that my background in the fast-paced and demanding advertising agency world remains a valuable and relevant experience as it relates to my current work as a Web Developer.</p>
             <p>The last position I held in advertising was at maxus in NYC where I led a team working primarily on Church & Dwight consumer brands. In that role as a Partner, Group Planning Director, I was the senior client contact responsible for strategic development and implementation oversight.</p>
-            <p>Over the course of my career, I gained extensive experience working in the digital advertising space, including social media strategy and custom program development, along with traditional media such as TV and print. Prior to my position at maxus in NYC from 1/08 to 8/11, I worked at MindShare (Chicago 9/05 - 3/07), Mediaedge NYC (3/03 - 8/05), and Brouillard Communications (NYC 9/00 - 3/03). All current or former WPP companies.</p>
+            <p>Along with traditional media like TV and print, I spent a significant amount of time working in the digital advertising space, while it emerged as critical advertising channel. This went beyond banner ad network buys, and included social media strategy as well as custom program development. Prior to my position at maxus in NYC from 1/08 to 8/11, I worked at MindShare (Chicago 9/05 - 3/07), Mediaedge NYC (3/03 - 8/05), and Brouillard Communications (NYC 9/00 - 3/03). All of which are current or former WPP companies.</p>
         </div>
     </section>
 
