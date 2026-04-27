852610f61528ca960c15454beff839a672b5159f
Andy Knapp <andyknapp@andys-air.lan>
2026-04-26 20:20:49 -0400

    chore: delete old site

--- archive/components/footer.php
+++
@@ -1,5 +0,0 @@
-<footer class="site-footer" role="contentinfo">
-    <ul>
-        <li class="headline-sub">&copy; <?php echo date('Y');?> | Andy Knapp</li>
-    </ul>
-</footer>
--- archive/components/nav.php
+++
@@ -1,6 +0,0 @@
-<nav class="site-nav">
-    <span class="nav-logo"><?php include('logo/ak.svg'); ?></span>
-    <a href="#work" class="nav-item nav-item-work">Work</a>
-    <a href="#experience" class="nav-item nav-item-exp">Experience</a>
-    <a href="#contact" class="nav-item nav-item-contact">Contact</a>
-</nav>
